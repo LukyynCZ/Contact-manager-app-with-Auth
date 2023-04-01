6770c257f9ee21706ecd38a7159dc9049450254d
Lukas <macku.lukas@gmail.com>
2023-04-01 11:59:17 +0200

    Added authorization and pwd hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,36 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
 //  @desc   Register a user
 //  @route  POST   /api/users/register
 //  @access public
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error('All fields are required!');
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error('User already registered');
+  }
+  // Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error('User data is not valid');
+  }
   res.json({ message: 'Register the user' });
 });
 
@@ -13,7 +39,31 @@ const registerUser = asyncHandler(async (req, res) => {
 //  @access public
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Login user' });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error('All fields are required!');
+  }
+  const user = await User.findOne({ email });
+
+  // Compare pwd with hashed pwd
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '2m' }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error('email or password is not valid');
+  }
 });
 
 //  @desc   Current user info
